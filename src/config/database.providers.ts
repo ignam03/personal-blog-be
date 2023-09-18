02bb9144e5242ed893a9bbf7e3d2662215077a18
Ignacio M <ignafree97@gmail.com>
2023-09-18 11:15:10 -0300

    ssl connect database

--- src/config/database.providers.ts
+++ src/config/database.providers.ts
@@ -20,6 +20,11 @@ export const databaseProviders = [
         username: configService.get<string>('DATABASE_USER'),
         password: configService.get<string>('DATABASE_PASSWORD'),
         database: configService.get<string>('DATABASE_NAME'),
+        ssl: configService.get<string>('DB_DIALECT') === 'true',
+        dialectOptions: {
+          ssl: true,
+          native: true,
+        },
       });
       sequelize.addModels([User, Article]);
       await sequelize.sync({ alter: true });
