4fbc22d917f83e6576ce2433028076ec6250bd5d
Ignacio M <ignafree97@gmail.com>
2024-03-11 02:02:45 -0300

    fix send mail

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -6,28 +6,39 @@ export class MailService {
   constructor(private mailService: MailerService) {}
 
   async sendUserConfirmation(email: string, name: string, token: string) {
-    const url = `${process.env.FRONTEND_ENDPOINT}confirm-account/${token}`;
-    await this.mailService.sendMail({
-      to: email,
-      subject: 'Welcome to Nice App! Confirm your Email',
-      template: 'confirmation',
-      context: {
-        name,
-        url,
-      },
-    });
+    try {
+      const url = `${process.env.FRONTEND_ENDPOINT}confirm-account/${token}`;
+      await this.mailService.sendMail({
+        to: email,
+        subject: 'Welcome to Nice App! Confirm your Email',
+        template: 'confirmation',
+        context: {
+          name,
+          url,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async sendPasswordReset(email: string, name: string, token: string) {
-    const url = `${process.env.FRONTEND_ENDPOINT}new-password/${token}`;
-    await this.mailService.sendMail({
-      to: email,
-      subject: 'Reset your Password',
-      template: 'reset',
-      context: {
-        name,
-        url,
-      },
-    });
+    try {
+      const url = `${process.env.FRONTEND_ENDPOINT}new-password/${token}`;
+      await this.mailService.sendMail({
+        to: email,
+        subject: 'Reset your Password',
+        template: 'reset',
+        context: {
+          name,
+          url,
+        },
+      });
+      return {
+        success: true,
+      };
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
